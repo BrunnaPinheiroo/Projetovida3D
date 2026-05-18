93d338d3628d996a34fcae21f26feb69a80bd3bf
BrunnaPinheiroo <bruna.gabrielli.pinheiro@escola.pr.gov.br>
2026-05-18 15:18:45 +0000

    aula.18-05

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-const botoes =  document.querySelectorAll(".botao"); 
-for(let i =0; i < botoes.length; i++){
+const botoes =  document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
+for (let i =0; i < botoes.length; i++){
+    botoes[i].onclick = function (){
+        for(let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo")
+        textos[i].classList.add("ativo")
+    }
 console.log(botoes);
 }
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo1 = new Date("2030-30-04T23:59:59");
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo2 = new Date("2026-11-10T23:59:59");
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo3 = new Date("2030-30-12T23:59:59");
+const contadores = document.querySelectorAll("contador");
+const tempoObjetivo4 = new Date("2027-30-04T23:59:59");
